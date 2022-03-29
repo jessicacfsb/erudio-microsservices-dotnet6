089ba94245c336d264390713cec0ee041141f1e8
jessica.cfsb <jessica.cfsb@gmail.com>
2022-03-29 16:21:26 -0300

    Adicionando imagens

---
+++ S08 - Organizando Arquitetura/GeekShooping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context {
+
+    public class MySQLContext : DbContext {
+        //2contrutores
+        public MySQLContext() { }
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+        
+        //Adicionar o DbSet de Produtos ao Mapeamento
+        public DbSet<Product> Products { get; set; }
+    }
+}
