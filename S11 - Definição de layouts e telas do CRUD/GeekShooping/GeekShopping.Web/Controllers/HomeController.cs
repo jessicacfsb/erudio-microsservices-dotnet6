d588c2c5be2e2aceab1688428e5ea67773076d35
jessica.cfsb <jessica.cfsb@gmail.com>
2022-04-06 16:21:42 -0300

    S11 - Implementando o login e logout / utilização dos tokens

--- S11 - Definição de layouts e telas do CRUD/GeekShooping/GeekShopping.Web/Controllers/HomeController.cs
+++ S11 - Definição de layouts e telas do CRUD/GeekShooping/GeekShopping.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using GeekShopping.Web.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -21,6 +23,17 @@ namespace GeekShopping.Web.Controllers {
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Login() {
+            //pegar o token
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            return RedirectToAction(nameof(Index));  //invocar o metodo
+        }
+
+        public IActionResult Logout() {
+            return SignOut("Cookies", "oidc");  //invocar o metodo
         }
     }
 }
