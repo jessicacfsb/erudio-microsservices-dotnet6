e0b12dd5d8c0c0457f3a1f17a2d25e52270052d6
jessica.cfsb <jessica.cfsb@gmail.com>
2022-04-25 10:09:25 -0300

    S14 - concluido

--- S14 - Consumindo o Carrinho de Compras/GeekShooping/GeekShopping.Web/Models/CartHeaderViewModel.cs
+++ S14 - Consumindo o Carrinho de Compras/GeekShooping/GeekShopping.Web/Models/CartHeaderViewModel.cs
@@ -5,5 +5,6 @@
         public long Id { get; set; }
         public string UserId { get; set; }
         public string? CuponCode { get; set; }
+        public decimal PurchaseAmount { get; set; }
     }
 }
