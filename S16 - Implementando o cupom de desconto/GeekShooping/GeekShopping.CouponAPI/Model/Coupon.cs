c8ba5711575f6ee61382d3cb2fc2aa96b825bfaa
jessica.cfsb <jessica.cfsb@gmail.com>
2022-06-03 11:16:33 -0300

    mapeando entidades

---
+++ S16 - Implementando o cupom de desconto/GeekShooping/GeekShopping.CouponAPI/Model/Coupon.cs
@@ -0,0 +1,23 @@
+﻿using GeekShopping.CouponAPI.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.CouponAPI.Model
+{
+
+    //Definir o nome da Tabela no banco
+    [Table("coupon")]
+    public class Coupon : BaseEntity{
+
+        //Definir as colunas
+        [Column("coupon_code")]
+        [Required]
+        [StringLength(30)] //Definir o tamanho máximo
+        public string CouponCode { get; set; }
+
+        [Column("discount_amount")]
+        [Required]
+        public decimal DiscountAmount { get; set; }
+
+    }
+}
